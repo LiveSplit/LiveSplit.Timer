aca18bd24e6980c5775b48656e75ce5f573525c7
Ero <56401411+just-ero@users.noreply.github.com>
2024-09-05 03:42:55 +0200

    Add `.editorconfig` & apply style rules (#15)
    * Add `.editorconfig`
    
    * Upgrade to C# 12
    
    * Use file-scoped namespaces, move `using` directives, fix whitespace
    
    * `IDE0005`, `IDE0065`, `IDE0161`
    
    * Add and order modifiers
    
    * `IDE0036`, `IDE0040`, `IDE0044`, `IDE0064`, `IDE0250`, `IDE0251`
    
    * Apply member body preferences
    
    * `IDE0021`, `IDE0022`, `IDE0023`, `IDE0024`, `IDE0025`, `IDE0026`, `IDE0027`, `IDE0053`, `IDE0061`
    
    * Apply braces, parentheses, new-line preferences
    
    * `IDE0011`, `IDE0047`, `IDE0048`, `IDE2000`, `IDE2001`, `IDE2002`, `IDE2003`, `IDE2004`, `IDE2005`, `IDE2006`
    
    * Use pattern matching
    
    * `IDE0019`, `IDE0020`, `IDE0038`, `IDE0078`, `IDE0083`, `IDE0170`, `IDE0260`
    
    * Inline variable declaration
    
    * `IDE0018`
    
    * Apply `var` preferences, simplify `new` expression
    
    * `IDE0007`, `IDE0008`, `IDE0090`
    
    * Use switch expression
    
    * `IDE0066`
    
    * Use index/range operator
    
    * `IDE0056`, `IDE0057`
    
    * Enforce code style in build

--- src/LiveSplit.Timer/UI/Components/TimerFactory.cs
+++ src/LiveSplit.Timer/UI/Components/TimerFactory.cs
@@ -1,27 +1,30 @@
-﻿using LiveSplit.Model;
+﻿using System;
+
+using LiveSplit.Model;
 using LiveSplit.UI.Components;
-using System;
 
 [assembly: ComponentFactory(typeof(TimerFactory))]
 
-namespace LiveSplit.UI.Components
+namespace LiveSplit.UI.Components;
+
+public class TimerFactory : IComponentFactory
 {
-    public class TimerFactory : IComponentFactory
+    public string ComponentName => "Timer";
+
+    public string Description => "Displays the current run time.";
+
+    public ComponentCategory Category => ComponentCategory.Timer;
+
+    public IComponent Create(LiveSplitState state)
     {
-        public string ComponentName => "Timer";
-
-        public string Description => "Displays the current run time.";
-
-        public ComponentCategory Category => ComponentCategory.Timer;
-
-        public IComponent Create(LiveSplitState state) => new Timer();
-
-        public string UpdateName => ComponentName;
-
-        public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.Timer.xml";
-
-        public string UpdateURL => "http://livesplit.org/update/";
-
-        public Version Version => Version.Parse("1.8.29");
+        return new Timer();
     }
+
+    public string UpdateName => ComponentName;
+
+    public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.Timer.xml";
+
+    public string UpdateURL => "http://livesplit.org/update/";
+
+    public Version Version => Version.Parse("1.8.29");
 }
